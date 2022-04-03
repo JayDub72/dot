c1d419e84cb4f046a88378f5126a4d2f0fa5a89e
Jason Worthen <jason@worthen.org>
2022-04-02 21:00:12 -0500

    ubuntu powerlevel10k install

--- src/os/installs/ubuntu/zsh.sh
+++ src/os/installs/ubuntu/zsh.sh
@@ -10,10 +10,12 @@ print_in_purple "\n   ZSH\n\n"
 
 install_package "ZSH" "zsh zsh-syntax-highlighting zsh-autosuggestions"
 
-print_in_purple "\n   Powerlevel9k\n\n"
+execute "sh -c \"$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)\"" \
+    "Installing Oh My Zsh"
 
-install_package "Powerlevel9K" "zsh-theme-powerlevel9k"
-
+execute "git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ~/powerlevel10k" \
+    "Installing Powerlevel10K"
+    
 execute "sleep 1" "change default shell"
 # hack until error fixed -- Password: chsh: PAM: Authentication failure
 chsh -s $(which zsh)
