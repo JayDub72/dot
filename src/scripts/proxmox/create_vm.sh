11fe574408c5580b13dc9c1dd8dd25778b823a95
Jason Worthen <jason@worthen.org>
2022-04-11 11:52:46 -0500

    handle error on vm stop and destory

--- src/scripts/proxmox/create_vm.sh
+++ src/scripts/proxmox/create_vm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # assign variables to inputs
 if [ $# != 0 ]
   then
@@ -13,6 +15,10 @@ if [ $# != 0 ]
     read -p "Enter new VM Name: " vmName
     read -p "Enter Cloud Init Template to use: " templateID
 fi
+
+# stop and destroy VM if it exists
+sudo qm stop "$vmID" || true
+sudo qm destroy "$vmID" || true
 
 sudo qm clone "$templateID" "$vmID" --name "$vmName"
 sudo qm resize "$vmID" scsi0 +10G
