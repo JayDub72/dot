7386c06d0564ad35cff16770e8adebce75ab13c9
Jason Worthen <jason@worthen.org>
2022-06-11 19:20:29 -0500

    update iterm install

--- src/os/preferences/macos/iterm.sh
+++ src/os/preferences/macos/iterm.sh
@@ -9,14 +9,12 @@ print_in_purple "\n   iTerm\n\n"
 
 # Force iTerm to check ~/.dotfiles/.iTerm2 directory for configuration
 defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "${HOME}/.dotfiles/src/os/preferences/macos/iterm2"
+
 execute "defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true" \
     "Force iTerm2 to check ~/.dotfiles/iterm2 directory for configuration"
 
 execute "defaults write com.googlecode.iterm2 PromptOnQuit -bool false" \
     "Don't display the annoying prompt when quitting iTerm2" 
-
-execute  "/usr/libexec/PlistBuddy -c "set \"New Bookmarks\":0:\"Custom Directory\" Recycle" ~/Library/Preferences/com.googlecode.iterm2.plist" \
-    "Make iTerm2 load new tabs in the same directory"
 
 execute "exec sh ${HOME}/.dotfiles/src/os/preferences/macos/iterm2/materialshell-dark.itermcolors" \
     "Install Material Theme for iTerm2" 
