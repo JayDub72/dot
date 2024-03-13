1520017b1b3c7c2481a0e76d2ea514845d08c2a2
Jason Worthen <jason@worthen.org>
2024-03-12 19:35:48 -0500

    remove app installs

--- src/os/installs/macos/apps.sh
+++ src/os/installs/macos/apps.sh
@@ -10,14 +10,16 @@ print_in_purple "\n   Applications\n\n"
 
 brew_install "1Password" "1password"
 brew_install "App Cleaner" "appcleaner"
-brew_install "Authy" "authy"
-brew_install "Backblaze" "backblaze"
+# brew_install "Authy" "authy" - deprecated on MacOS
+# brew_install "Backblaze" "backblaze" - brew install largely useless
 brew_install "Discord" "discord"
 brew_install "Google Drive" "google-drive"
+brew_install "Hidden Bar" "hiddenbar"
 brew_install "iTerm2" "iterm2"
 brew_install "iVPN" "ivpn"
 brew_install "Logos" "logos"
 brew_install "Microsoft Office" "microsoft-office"
+brew_install "NordVPN" "nordvpn"
 brew_install "Notion.so" "notion"
 brew_install "Omni Disk Sweeper" "omnidisksweeper"
 brew_install "Plex Media Player" "plex-media-player"
